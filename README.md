ba0e7fa2fb9f8e999921c44475af0d30fed9c0d7
Blaine Schanfeldt <git@blaines.me>
2013-04-08 11:18:26 -0700

    Added a porcelain command, DRY-ed CLI and updated readme

--- README.md
+++ README.md
@@ -2,11 +2,37 @@
 
 Grades your changes based on best practices.
 
+## Installation
+
+    git clone git@source.flexilis.local:bschanfeldt/Professor.git
+    cd Professor
+    gem build professor.gemspec; gem install professor-0.0.1.pre.gem
+    professor help
+
 ## Usage
 
-    gem install professor
-    professor grade
+Today the professor can help in three ways _everything_, _changes_, and _commit_
 
 ### Grade
 
-Checks the last commit against best practices
+    professor grade
+
+A porcelain command to check changes or last commit based on the repo status.
+
+#### Grade Everything
+
+    professor grade everything
+
+Grades the entire codebase, by default targets only ruby files in lib and app.
+
+#### Grade Changes
+
+    professor grade changes
+
+Grades the files changed since last commit
+
+#### Grade Commit
+
+    professor grade commit
+
+Grades the files changed in the most recent commit
--- lib/professor/cli.rb
+++ lib/professor/cli.rb
@@ -10,30 +10,50 @@ module Professor
       puts "Professor"
     end
 
-    desc "exec", "exec"
-    def exec
-      puts "Professor running"
+    desc "grade", "grade"
+    def grade(what_to_grade=nil)
+      case what_to_grade
+      when /everything/i
+        files_to_grade = grade_everything
+      when /changes/i
+        files_to_grade = grade_changes
+      when /commit/i
+        files_to_grade = grade_commit
+      when nil
+        files_to_grade = grade_porcelain
+      end
       spec = {
         :max_violations=>0,
         :parallel=>false,
         :exclusions_file=>nil,
         :checks=>[RubyBestPractices],
-        :rbp_glob=>"{app,lib}/**/*.rb"
+        :rbp_glob=>files_to_grade
       }
       Cane.run(spec)
     end
 
-    desc "grade", "grade"
-    def grade
-      comm = SystemHelper.show_exec("git diff-tree --no-commit-id --name-only -r HEAD")
-      spec = {
-        :max_violations=>0,
-        :parallel=>false,
-        :exclusions_file=>nil,
-        :checks=>[RubyBestPractices],
-        :rbp_glob=>comm.stdout.split.reject {|s| !(s =~ /\.rb/)}
-      }
-      Cane.run(spec)
+    no_tasks do
+      def grade_everything
+        files_to_grade ||= "{app,lib}/**/*.rb"
+      end
+
+      def grade_changes
+        changed_files = SystemHelper.show_exec("git diff-index --name-only HEAD").stdout
+        files_to_grade ||= changed_files.split.reject {|s| !(s =~ /\.rb/)}
+      end
+
+      def grade_commit
+        changed_files = SystemHelper.show_exec("git diff-tree --no-commit-id --name-only -r HEAD").stdout
+        files_to_grade ||= changed_files.split.reject {|s| !(s =~ /\.rb/)}
+      end
+
+      def grade_porcelain
+        if SystemHelper.show_exec("git status --porcelain").stdout.empty?
+          grade_commit
+        else
+          grade_changes
+        end
+      end
     end
   end
 end
