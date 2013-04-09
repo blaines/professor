58cdde094e1bcaa378beb67a33476bf40b53b99d
Blaine Schanfeldt <git@blaines.me>
2013-04-09 16:56:11 -0700

    Removed Gemfile.lock and bumped version to something acceptable for bundler (can't handle 0)

--- lib/professor/version.rb
+++ lib/professor/version.rb
@@ -2,7 +2,7 @@ module Professor
   # http://semver.org/
   # http://rubygems.rubyforge.org/rubygems-update/Gem/Version.html
   MAJOR = ENV['MAJOR'] || 0
-  MINOR = ENV['MINOR'] || 0
+  MINOR = ENV['MINOR'] || 1
   PATCH = ENV['PATCH'] || 0
   VERSION = ENV['RELEASE'] ? "#{MAJOR}.#{MINOR}.#{PATCH}" : "#{MAJOR}.#{MINOR}.#{PATCH}.pre#{ENV['BUILD_NUMBER']}"
 end
