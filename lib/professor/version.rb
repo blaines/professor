0ec61c6fa8f0ecc09ad91522825105069e382233
Blaine Schanfeldt <git@blaines.me>
2013-04-09 16:49:09 -0700

    An experiment in semantic versioning

--- lib/professor/version.rb
+++ lib/professor/version.rb
@@ -1,3 +1,8 @@
 module Professor
-  VERSION = '0.0.1.pre'
+  # http://semver.org/
+  # http://rubygems.rubyforge.org/rubygems-update/Gem/Version.html
+  MAJOR = ENV['MAJOR'] || 0
+  MINOR = ENV['MINOR'] || 0
+  PATCH = ENV['PATCH'] || 0
+  VERSION = ENV['RELEASE'] ? "#{MAJOR}.#{MINOR}.#{PATCH}" : "#{MAJOR}.#{MINOR}.#{PATCH}.pre#{ENV['BUILD_NUMBER']}"
 end
