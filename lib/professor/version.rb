8348629e0a12e3fcb5d3b8413a6ee8c89adcea0f
Blaine Schanfeldt <git@blaines.me>
2013-04-11 17:26:12 -0700

    Version fix

--- lib/professor/version.rb
+++ lib/professor/version.rb
@@ -3,6 +3,6 @@ module Professor
   # http://rubygems.rubyforge.org/rubygems-update/Gem/Version.html
   MAJOR = 0
   MINOR = 1
-  PATCH = 0
-  VERSION = ENV['RELEASE'] == "true" ? "#{MAJOR}.#{MINOR}.#{PATCH}" : "#{MAJOR}.#{MINOR}.#{PATCH+1}.pre#{ENV['BUILD_NUMBER']}"
+  PATCH = 1
+  VERSION = ENV['RELEASE'] == "true" ? "#{MAJOR}.#{MINOR}.#{PATCH}" : "#{MAJOR}.#{MINOR}.#{PATCH}.pre#{ENV['BUILD_NUMBER']}"
 end
