f5c1233406732c30912a2a1c5c3c4dc274a382b1
Blaine Schanfeldt <git@blaines.me>
2013-04-11 18:30:23 -0700

    Version bump 0.1.2

--- lib/professor/version.rb
+++ lib/professor/version.rb
@@ -3,6 +3,6 @@ module Professor
   # http://rubygems.rubyforge.org/rubygems-update/Gem/Version.html
   MAJOR = 0
   MINOR = 1
-  PATCH = 1
+  PATCH = 2
   VERSION = ENV['RELEASE'] == "true" ? "#{MAJOR}.#{MINOR}.#{PATCH}" : "#{MAJOR}.#{MINOR}.#{PATCH}.pre#{ENV['BUILD_NUMBER']}"
 end
