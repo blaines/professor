524a7c92127afecc3f05af681915f39b5b7a13f6
Blaine Schanfeldt <git@blaines.me>
2013-04-17 13:49:39 -0700

    0.1.3 released, next up 0.1.4

--- lib/professor/version.rb
+++ lib/professor/version.rb
@@ -3,6 +3,6 @@ module Professor
   # http://rubygems.rubyforge.org/rubygems-update/Gem/Version.html
   MAJOR = 0
   MINOR = 1
-  PATCH = 3
+  PATCH = 4
   VERSION = ENV['RELEASE'] == "true" ? "#{MAJOR}.#{MINOR}.#{PATCH}" : "#{MAJOR}.#{MINOR}.#{PATCH}.pre#{ENV['BUILD_NUMBER']}"
 end
